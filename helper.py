017cd8935b55f086946ccfc1d8f149806ae50e67
Leoard Lupin-Jimenez <lupified@gmail.com>
2019-12-04 12:14:49 -0800

    pythia_func now includes way to get input parameter arguments, used for catching certain error types

--- helper.py
+++ helper.py
@@ -1,8 +1,12 @@
-def helper(info = 'No info provided.'):
+from inspect import signature
+
+def helper(info = '(No info provided)'):
     def should_help(func):
         def pythia_func(*args, **kwargs):
             if args[0] == "__help__":
                 return info
+            elif args[0] == "__params__":
+                return signature(func).parameters
             else:
                 return func(*args, **kwargs)
         return pythia_func
