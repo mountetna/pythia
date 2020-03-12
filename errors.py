41158ef870b5a0f3a40585671ef8f264077dc887
Lenny Lupin-Jimenez <lupified@gmail.com>
2020-03-12 20:51:56 +0000

    BaseError -> PythiaError

--- errors.py
+++ errors.py
@@ -1,20 +1,20 @@
-class BaseError(Exception):
+class PythiaError(Exception):
     def __init__(self, msg = "", status = 500):
         self.msg = msg
         self.status = status
         
-class Forbidden(BaseError):
+class Forbidden(PythiaError):
     def __init__(self, msg = "Action not permitted", status = 403):
         super(Forbidden, self).__init__(msg, status)
  
-class Unauthorized(BaseError):
+class Unauthorized(PythiaError):
     def __init__(self, msg = "Unauthorized", status = 401):
         super(Unauthorized, self).__init__(msg, status)
         
-class BadRequest(BaseError):
+class BadRequest(PythiaError):
     def __init__(self, msg = "Client error", status = 422):
-        super(BadRequest, self).__init__(msg, status)
+        super().__init__(msg, status)
         
-class ServerError(BaseError):
+class ServerError(PythiaError):
     def __init__(self, msg = "Server error", status = 500):
         super(ServerError, self).__init__(msg, status)
